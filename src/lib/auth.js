daeb435f9b8ad4fa878d0b43ab1d201cd9ecd49c
Ahmad Said <mail.saydorrahman@gmail.com>
2026-06-18 23:05:26 +0600

    solve it

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -13,7 +13,7 @@ export const auth = betterAuth({
     emailAndPassword: { 
     enabled: true, 
   },
-  trustedOrigins:process.env.BETTER_AUTH_URL,
+  trustedOrigins:"https://fullstack-final-client-project.vercel.app/",
   socialProviders: {
         google: { 
             clientId: process.env.GOOGLE_CLIENT_ID , 
